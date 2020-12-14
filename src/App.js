4214cc247f1744058d3f020ffc9c64b854b70f9d
Shivam Soni <shivamsoni1737@gmail.com>
2020-12-14 12:26:06 +0530

    added temporary banner and some modifications

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header.js';
 import Navbar from './navbar.js';
+import Home from './Home.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function home() {
+    return (
+        <div className="home">
+            <img className="home__image" src="https://cdn.igp.com/f_auto,q_auto/banners/banner-new-year-2020.jpg" alt="" />
+        </div>
+    )
+}
+
+export default home;
